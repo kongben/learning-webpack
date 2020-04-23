dd668c00c17ddde20e60830837ca8a1efc932179
kongben <1317495859@qq.com>
2020-04-23 08:49:48 +0800

    代码分割 js懒加载

--- src/search/index.js
+++ src/search/index.js
@@ -6,13 +6,38 @@ import logo from '../assets/avatar.png'
 import "../../common";
 import { a } from "./tree-sharking";
 class Search extends React.Component {
+
+    constructor() {
+        super(...arguments);
+
+        this.state = {
+            Text: null
+        }
+    }
+
+    loadComponent() {
+        import('./test.js').then((Text) => {
+            this.setState({
+                Text: Text.default
+            })
+        })
+    }
+
     render() {
         const funcA = a()
-        return <div className='search-text'>{funcA} <img src={ logo }></img>Search Text</div>
+        const { Text } = this.state
+        
+        return <div className='search-text'>
+            {
+                Text ? <Text /> : null
+            }
+            {funcA} <img src={logo} onClick={this.loadComponent.bind(this)}></img>
+            Search Text
+            </div>
     }
 }
 
 ReactDOM.render(
-    <Search/>,
+    <Search />,
     document.getElementById('root')
 )
